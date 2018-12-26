c25064d22e7c2b7446c9f9b19cb208fd7a11f6a0
Bo Cao <caobo.github@gmail.com>
2018-12-26 13:03:00 -0500

    writeyaml half finished

---
+++ writeyaml.cc
@@ -0,0 +1,31 @@
+#include "writeyaml.h"
+
+#define SIMPLEWRITE(T)
+template <class T>
+YAML::Node writeYAML(const T& content)
+{
+  return writeYAMLSimple(content);
+}
+template
+YAML::Node writeYAML<T>(const T& content);
+
+SIMPLEWRITE(bool)
+
+SIMPLEWRITE(int8_t)
+SIMPLEWRITE(int16_t)
+SIMPLEWRITE(int32_t)
+SIMPLEWRITE(int64_t)
+
+SIMPLEWRITE(uint8_t)
+SIMPLEWRITE(uint16_t)
+SIMPLEWRITE(uint32_t)
+SIMPLEWRITE(uint64_t)
+
+SIMPLEWRITE(float)
+SIMPLEWRITE(double)
+SIMPLEWRITE(long double)
+
+SIMPLEWRITE(char)
+SIMPLEWRITE(std::string)
+
+using namespace std;
--- writeyaml.h
+++ writeyaml.h
@@ -1,4 +1,71 @@
 #ifndef WRITEYAML_RWYAML_H
 #define WRITEYAML_RWYAML_H
 
+#include "common.h"
+#include "basic_functions.h"
+
+
+//Declarations
+template <class T>
+YAML::Node writeYAML(const T& content);
+
+template <class T>
+YAML::Node writeYAMLSimple(const T& content);
+
+template <class T, template <class> class LIST>
+YAML::Node writeYAMLList(const LIST<T>& content);
+
+template <class T, class It>
+YAML::Node writeYAMLList(It begin, It end);
+
+template <class K, class V, template<class> class MAP>
+YAML::Node writeYAMLMap(const MAP<T>& content);
+
+template <class T, class It>
+YAML::Node writeYAMLMap(It begin, It end);
+
+//Definitions
+template <class T>
+YAML::Node writeYAMLSimple(const T& content)
+{
+  return YAML::Node(std::to_string(content));
+}
+
+template <class T, template <class> class LIST>
+YAML::Node writeYAMLList(const LIST<T>& content)
+{
+  return writeYAMLList(content.begin(), content.end());
+}
+
+template <class It>
+YAML::Node writeYAMLList(It begin, It end)
+{
+  YAML::Node node;
+  size_t count = 0;
+  for (auto x = begin; x != end; ++x)
+  {
+    node[count] = writeYAML(*It);
+    ++count;
+  }
+  return node;
+}
+
+template <class K, class V, template<class> class MAP>
+YAML::Node writeYAMLMap(const MAP<T>& content)
+{
+  return writeYAMLMap(content.begin(), content.end());
+}
+
+template <class It>
+YAML::Node writeYAMLMap(It begin, It end)
+{
+  YAML::Node node;
+  for (auto x = begin; x != end; ++x)
+  {
+    node[std::to_string(x->first)] = writeYAML(x->second);
+  }
+  return node;
+}
+
+
 #endif
